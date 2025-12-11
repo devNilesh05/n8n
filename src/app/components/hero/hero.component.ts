effb029241452c468e036f0b31ff71d2c0064324
MANISH <khandagalemanish21@gmail.com>
2025-12-11 17:38:43 +0530

    hero section done

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatButton } from "@angular/material/button";
-
+import gsap from 'gsap';
 @Component({
   selector: 'app-hero',
   imports: [MatButton],
@@ -14,10 +14,34 @@ ngAfterViewInit() {
     video.muted = true;
     video.play().catch(err => console.log('autoplay blocked',err))
     const t2 = gsap.timeline()
+    t2.from('.leftsection',{
+      opacity:0,
+      x:-50,
+      duration:1,
+      ease:'power1.in'
+    })
+    t2.from('.rightsection',{
+      opacity:0,
+      x:50,
+      duration:1,
+      ease:'power1.in'
+    })
+    t2.from('.started',{
+      opacity:0,
+      y:30,
+      duration:1,
+      ease:'power1.in'
+    })
+    t2.from('.talk',{
+      opacity:0,
+      y:-30,
+      duration:1,
+      ease:'power1.in'
+    })
     t2.from('.tag',{
       color:'white',
       duration:1,
-      ease:'back.in',
+      ease:'bounce.in',
       repeat:-1,
       yoyo:true
     })
